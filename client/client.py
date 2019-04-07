f1b78bc725a18fb762cc19f499fb86a44d4c51ac
zargarzadehm <moein.zargarzadeh@gmail.com>
2019-04-07 13:43:52 +0430

    Complete Server And Client

---
+++ client/client.py
@@ -0,0 +1,233 @@
+import requests
+import os
+import time
+import platform
+import sys
+import json
+PARAMS = CMD = USERNAME = PASSWORD = API = ROLE = ""
+HOST = "127.0.0.1"
+PORT = "1104"
+
+
+def __postcr__():
+    return "http://"+HOST+":"+PORT+"/"+CMD+"?"
+
+
+def __api__():
+    return "http://" + HOST + ":" + PORT + "/" + CMD + "/" + API
+
+
+def __authgetcr__():
+    return "http://"+HOST+":"+PORT+"/"+CMD+"/"+USERNAME+"/"+PASSWORD
+
+
+def print_status_ticket(r):
+    print(str(r['message']))
+
+
+def print_tickets(r):
+    z = 0
+    num = int(r["tickets"].split('-')[1])
+    ID = ''
+    if num == 0:
+        ID = 'back'
+    else:
+        while True:
+            clear()
+            x = []
+            z = 0
+            print('SHOW TICKETS : \n')
+            while z < num:
+                data = r["block {}".format(z)]
+                print(str(data["id"]) + " - " + str(data["subject"]) + " @ " + str(data["date"]) + " \n")
+                print("TICKET IS -->   " + str(data["body"]) + ":")
+                if not str(data["answer"]) == 'None':
+                    print("- ANSWER IS :" + str(data["answer"]))
+                print("\n")
+                print("-"*20)
+                x.append(str(data["id"]))
+                z += 1
+            print("ENTER ID TICKET FOR BACK ENTER back : ")
+            ID = sys.stdin.readline()[:-1]
+            if ID in x:
+                break
+            elif ID == 'back':
+                break
+            else:
+                print("ID IS INCORRECT TRY AGAIN")
+                time.sleep(2)
+
+    return str(ID)
+
+
+def show_funk_ticket():
+    print("ID TICKET : " + ID + "\n")
+    if ROLE == '1' :
+        print("""What Do You Prefer To Do :
+        1. Answer Ticket
+        2. Change Status Ticket
+        3. Back
+        """)
+    else:
+        print("""What Do You Prefer To Do :
+        1. Close Ticket
+        2. Back
+        """)
+
+
+def clear():
+    if platform.system() == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+def show_func():
+    if ROLE == '2':
+        print("USERNAME : "+USERNAME+"\n"+"API : " + API + "\n" + "!YOU ARE NORMAL USER !")
+        print("""What Do You Prefer To Do :
+        1. Send Ticket
+        2. Show Tickets
+        3. Logout
+        4. Exit
+        """)
+    elif ROLE == '1':
+        print("USERNAME : "+USERNAME+"\n"+"API : " + API + "\n" + "!YOU ARE ADMIN !")
+        print("""What Do You Prefer To Do :
+        1. Send Ticket
+        2. Show Tickets
+        3. Logout
+        4. Exit
+        """)
+
+
+while True:
+    clear()
+    print("""WELCOME TO TICKET CLIENT
+    Please Choose What You Want To Do :
+    1. signin
+    2. signup
+    3. exit
+    """)
+    status = sys.stdin.readline()
+    if status[:-1] == '1':
+        clear()
+        while True:
+            print("USERNAME : ")
+            USERNAME = sys.stdin.readline()[:-1]
+            print("PASSWORD : ")
+            PASSWORD = sys.stdin.readline()[:-1]
+            CMD = "login"
+            r = requests.get(__authgetcr__()).json()
+            if str(r['status']) == '200':
+                clear()
+                print("USERNAME AND PASSWORD IS CORRECT\nLogging You in ...")
+                API = str(r['api'])
+                ROLE = str(r['role'])
+                time.sleep(1)
+                break
+            else:
+                clear()
+                print("USERNAME AND PASSWORD IS INCORRECT\nTRY AGAIN ...")
+                time.sleep(1)
+        while True:
+            clear()
+            show_func()
+            func_type = sys.stdin.readline()
+            if func_type[:-1] == '1':
+                clear()
+                CMD = "sendticket"
+                print("ENTER SUBJECT TICKETS : ")
+                SUBJECT = sys.stdin.readline()[:-1]
+                print("ENTER TICKET : ")
+                BODY = sys.stdin.readline()[:-1]
+                PARAMS = {'api': API, 'subject': SUBJECT, 'body': BODY}
+                data = requests.post(__postcr__(), PARAMS).json()
+                print_status_ticket(data)
+                input("Press Any Key To Continue ...")
+            if func_type[:-1] == '2':
+                while True:
+                    clear()
+                    if ROLE == '1':
+                        CMD = "getticketmod"
+                    elif ROLE == '2':
+                        CMD = "getticketcli"
+
+                    data = requests.get(__api__()).json()
+
+                    ID = print_tickets(data)
+                    print(ID)
+                    if ID == "back":
+                        break
+                    else:
+                        while True:
+                            clear()
+                            show_funk_ticket()
+                            func_type = sys.stdin.readline()
+                            if ROLE == '1':
+                                if func_type[:-1] == '1':
+                                    clear()
+                                    CMD = "restoticketmod"
+                                    print("ENTER ANSWER : ")
+                                    ANSWER = sys.stdin.readline()[:-1]
+                                    PARAMS = {'api': API, 'id': int(ID), 'answer': ANSWER}
+                                    data = requests.post(__postcr__(), PARAMS).json()
+                                    print_status_ticket(data)
+                                    input("Press Any Key To Continue ...")
+                                elif func_type[:-1] == '2':
+                                    clear()
+                                    CMD = "changestatus"
+                                    print("ENTER STATUS : ")
+                                    STATUS = sys.stdin.readline()[:-1]
+                                    PARAMS = {'api': API, 'id': int(ID), 'status': STATUS}
+                                    data = requests.post(__postcr__(), PARAMS).json()
+                                    print_status_ticket(data)
+                                    input("Press Any Key To Continue ...")
+                                else:
+                                    break
+                            else:
+                                if func_type[:-1] == '1':
+                                    clear()
+                                    CMD = "closeticket"
+                                    PARAMS = {'api': API, 'id': ID}
+                                    data = requests.post(__postcr__(), PARAMS).json()
+                                    print_status_ticket(data)
+                                    input("Press Any Key To Continue ...")
+                                elif func_type[:-1] == '2':
+                                    break
+
+            elif func_type[:-1] == '3':
+                break
+            elif func_type[:-1] == '4':
+                sys.exit()
+
+    elif status[:-1] == '2':
+        clear()
+        while True:
+            print("To Create New Account Enter The Specifications")
+            print("USERNAME : ")
+            USERNAME = sys.stdin.readline()[:-1]
+            print("PASSWORD : ")
+            PASSWORD = sys.stdin.readline()[:-1]
+            print("FIRST NAME : ")
+            FIRST_NAME = sys.stdin.readline()[:-1]
+            print("LAST NAME :")
+            LAST_NAME = sys.stdin.readline()[:-1]
+            CMD = "signup"
+            clear()
+            PARAMS = {'username': USERNAME, 'password': PASSWORD,
+                      'first_name': FIRST_NAME, 'last_name': LAST_NAME}
+            r = requests.post(__postcr__(), PARAMS).json()
+            if str(r['status']) == "200":
+                print("Your Acount Is Created\n"+"Your Username :"+USERNAME+"\nYour API : "+str(r['api']))
+                time.sleep(2)
+                break
+            else:
+                print(r['status']+"\n"+"Try Again")
+                input("Press Any Key To Continue ...")
+                clear()
+
+    elif status[:-1] == '3':
+        sys.exit()
+    else:
+        print("Wrong Choose Try Again")
---
+++ server/server.py
@@ -0,0 +1,290 @@
+import os.path
+
+import tornado.escape
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import os
+from binascii import hexlify
+import tornado.web
+from tornado.options import define, options
+import mysql.connector
+import datetime
+import json
+
+define("port", default=1104, help="run on the given port", type=int)
+define("mysql_host", default="127.0.0.1", help="database host")
+define("mysql_database", default="tickets", help="database name")
+define("mysql_user", default="admin", help="database user")
+define("mysql_password", default="12345689", help="database password")
+
+mydb = mysql.connector.connect(
+    host=options.mysql_host,
+    user=options.mysql_user,
+    passwd=options.mysql_password,
+    database=options.mysql_database
+)
+mycursor = mydb.cursor(dictionary=True, buffered=True)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        # GET METHOD :
+        handlers = [
+            (r"/getticketmod/([^/]+)", getticketmod), #Balance Using API Format : /getticketmod/API
+            (r"/getticketcli/([^/]+)", getticketcli),  # Balance Using API Format : /getticketcli/API
+            (r"/login/([^/]+)/([^/]+)", login),
+            (r"/logout/([^/]+)/([^/]+)", logout),
+            # POST METHOD :
+            (r"/signup", signup),
+            (r"/sendticket", sendticket),
+            (r"/restoticketmod", restoticketmod),
+            (r"/changestatus", changestatus),  # Balance Using API Format : /changestatus/token/id/status
+            (r"/closeticket", closeticket),# Balance Using Authentication Format : /closeticket/token/id
+            (r".*", defaulthandler),
+        ]
+        settings = dict()
+        super(Application, self).__init__(handlers, **settings)
+        # self.db = torndb.Connection(
+        #     host=options.mysql_host, database=options.mysql_database,
+        #     user=options.mysql_user, password=options.mysql_password)
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    def check_user(self,user):
+        mycursor.execute("SELECT * from users where username = %s", (user,))
+        resuser = mycursor.fetchone()
+        if resuser:
+            return True
+        else:
+            return False
+
+    def check_api(self, api):
+        mycursor.execute("SELECT * from users where api = %s", (api,))
+        resuser = mycursor.fetchone()
+        if resuser:
+            return resuser['username']
+        else:
+            return False
+
+    def check_auth(self, username, password):
+        mycursor.execute("SELECT * from users where username = %s and password = %s", (username, password))
+        resuser = mycursor.fetchone()
+        if resuser:
+            return True
+        else:
+            return False
+
+    def check_role(self, api):
+        mycursor.execute("SELECT * from users where api = %s", (api,))
+        user = mycursor.fetchone()
+        if user['role'] == 1:
+            return '1'
+        elif user['role'] == 2:
+            return '2'
+
+class defaulthandler(BaseHandler):
+    def get(self):
+        output = {'status': 'Wrong Command'}
+        self.write(output)
+
+    def post(self, *args, **kwargs):
+        output = {'status':'Wrong Command'}
+        self.write(output)
+
+
+class signup(BaseHandler):
+    def post(self, *args, **kwargs):
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        first_name = self.get_argument('first_name')
+        last_name = self.get_argument('last_name')
+        if not self.check_user(username):
+            api_token = str(hexlify(os.urandom(16)).decode())
+            mycursor.execute("INSERT INTO users (username, password, first_name, last_name, api)"
+                             " VALUES (%s,%s,%s,%s,%s) ", (username, password, first_name, last_name, api_token))
+            mydb.commit()
+            user_id = mycursor.lastrowid
+
+            output = {"message": "Signed Up Successfully",
+                      "api": api_token,
+                      "status": '200'}
+            self.write(output)
+        else:
+            output = {'status': '401'}
+            self.write(output)
+
+
+class login(BaseHandler):
+    def get(self, *args, **kwargs):
+        if self.check_auth(args[0], args[1]):
+            mycursor.execute("SELECT * from users where username = %s and password = %s", (args[0], args[1]))
+            user = mycursor.fetchone()
+            output = {'status': '200',
+                      'api': user['api'],
+                      'role': user['role'],
+                      'message': 'Logged in Successfully'}
+            self.write(output)
+        else:
+            output = {'status': '401'}
+            self.write(output)
+
+
+class logout(BaseHandler):
+    def get(self, *args, **kwargs):
+        if self.check_auth(args[0], args[1]):
+            output = {'status': '200',
+                      'message': 'Logged Out Successfully'}
+            self.write(output)
+        else:
+            output = {'status': '401'}
+            self.write(output)
+
+
+class getticketcli(BaseHandler):
+    def get(self, *args, **kwargs):
+        if self.check_role(args[0]) == '2':
+            username = self.check_api(args[0])
+            mycursor.execute("SELECT * from tickets where username = %s", (username, ))
+            ticket = mycursor.fetchall()
+            number_ticket = len(ticket)
+
+            def myconverter(o):
+                if isinstance(o, datetime.datetime):
+                    return o.__str__()
+
+            if number_ticket == '0':
+                output = {'status': '401'}
+                self.write(output)
+            else:
+                output = {
+                    "tickets": "There Are -" + str(number_ticket) + "- Ticket",
+                    "code": "200",
+                }
+                y = 0
+                for x in ticket:
+                    output['block ' + str(y)] = x
+                    y += 1
+                self.write(json.dumps(output, default=myconverter))
+
+
+class getticketmod(BaseHandler):
+    def get(self, *args, **kwargs):
+        if self.check_role(args[0]) == '1':
+            mycursor.execute("SELECT * from tickets")
+            ticket = mycursor.fetchall()
+            number_ticket = len(ticket)
+
+            if number_ticket == '0':
+                output = {'status': '401'}
+                self.write(output)
+            else:
+                output = {
+                    "tickets": "There Are -" + str(number_ticket) + "- Ticket",
+                    "code": "200",
+                }
+                y = 0
+                for x in ticket:
+
+                    a = {}
+                    a["subject"] = x["subject"]
+                    a["body"] = x["body"]
+                    a["id"] = x["id"]
+                    a["answer"] = x["answer"]
+                    a["date"] = str(x["date"])
+                    a["status"] = x["status"]
+                    output['block ' + str(y)] = a
+                    y += 1
+                print(output)
+                self.write(output)
+
+
+class sendticket(BaseHandler):
+    def post(self, *args, **kwargs):
+        token = self.get_argument('api')
+        subject = self.get_argument('subject')
+        body = self.get_argument('body')
+        username = self.check_api(token)
+        if self.check_user(username):
+            mycursor.execute("INSERT INTO tickets (username, subject, body, status,date)"
+                             " VALUES (%s,%s,%s,%s,%s) ", (username, subject, body, 'open', datetime.datetime.now()))
+            mydb.commit()
+            user_id = mycursor.lastrowid
+
+            output = {"message": "Ticket Sent Successfully",
+                      "id": str(user_id), "code": "200"}
+            self.write(output)
+        else:
+            output = {'status': '401'}
+            self.write(output)
+
+
+class restoticketmod(BaseHandler):
+    def post(self, *args, **kwargs):
+        token = self.get_argument('api')
+        id = self.get_argument('id')
+        answer = self.get_argument('answer')
+        username = self.check_api(token)
+        if self.check_user(username):
+            mycursor.execute("UPDATE tickets set answer= %s where id = %s", (answer, id))
+            mydb.commit()
+
+            output = {
+                "message": "Response to Ticket With id -" + str(id) + "- Sent Successfully",
+                "code": "200",
+            }
+            self.write(output)
+        else:
+            output = {'status': '401'}
+            self.write(output)
+
+
+class changestatus(BaseHandler):
+    def post(self, *args, **kwargs):
+        token = self.get_argument('api')
+        id = self.get_argument('id')
+        status = self.get_argument('status')
+        username = self.check_api(token)
+        if self.check_user(username):
+            mycursor.execute("UPDATE tickets SET status = %s where id = %s", (status, id))
+            mydb.commit()
+
+            output = {
+                        "message": "Status Ticket With id -" + str(id) + "- Changed Successfully",
+                        "code": "200"
+                }
+            self.write(output)
+        else:
+            output = {'status': '401'}
+            self.write(output)
+
+
+class closeticket(BaseHandler):
+    def post(self, *args, **kwargs):
+        token = self.get_argument('api')
+        id = self.get_argument('id')
+        username = self.check_api(token)
+        if self.check_user(username):
+            mycursor.execute("UPDATE tickets SET status = %s where id = %s", ('Close', id))
+            mydb.commit()
+
+            output = {
+                "message": "Status Ticket With id -" + str(id) + "- Closed Successfully",
+                "code": "200"
+            }
+            self.write(output)
+        else:
+            output = {'status': '401'}
+            self.write(output)
+
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
+
